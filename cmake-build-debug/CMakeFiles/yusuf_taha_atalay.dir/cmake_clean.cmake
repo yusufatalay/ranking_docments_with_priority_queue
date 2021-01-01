4769f2a50dd9f31ae54cadb6ccee8c9251c096e4
Yusuf Taha ATALAY <yusuftahaatalay14@gmail.com>
2021-01-01 12:59:00 +0300

    initialized the project folder

---
+++ cmake-build-debug/CMakeFiles/yusuf_taha_atalay.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yusuf_taha_atalay.dir/yusuf_taha_atalay.c.o"
+  "yusuf_taha_atalay"
+  "yusuf_taha_atalay.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/yusuf_taha_atalay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/yusuf_taha_atalay.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for yusuf_taha_atalay.
+# This may be replaced when dependencies are built.
